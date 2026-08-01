b9103cd8042169eb218d2eb28271d09c354711e8
puchuanyuan9 <puchuanyuan9@gmail.com>
2026-08-01 14:19:09 +0900

    [add]ジャイロ操作を可能にした＋ProBuilderを試した

---
+++ Assets/Project/Scripts/CubeTiltController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem; 
+
+public class CubeTiltController : MonoBehaviour
+{
+    [Header("‰ñ“]İ’è")]
+    [SerializeField] private float maxTiltAngle = 20f; // Å‘åŒXÎŠp
+    [SerializeField] private float rotationSpeed = 5f; // ‰ñ“]‘¬“x
+
+    private Vector2 tiltInput;
+
+    public void OnTilt(InputValue value)
+    {
+        //InputSystem‚©‚ç‚Ì“ü—Í‚ğæ“¾‚µ‚ÄAtiltInput‚ÉŠi”[‚·‚é
+        tiltInput = value.Get<Vector2>();
+    }
+
+    void Update()
+    {
+        float targetX = -tiltInput.y * maxTiltAngle; // ã‰º‚ÌŒX‚«
+        float targetZ = -tiltInput.x * maxTiltAngle; // ¶‰E‚ÌŒX‚«i”½“]‚³‚¹‚éj
+
+        Quaternion targetRotation = Quaternion.Euler(targetX, 0f, targetZ);
+
+        transform.localRotation = Quaternion.Lerp(
+            transform.localRotation,
+            targetRotation,
+            rotationSpeed * Time.deltaTime
+        );
+    }
+}
