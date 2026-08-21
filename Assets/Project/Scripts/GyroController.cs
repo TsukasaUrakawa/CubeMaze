22b4ae0df0e2092a98382be210f74740e86f3842
puchuanyuan9 <puchuanyuan9@gmail.com>
2026-08-21 11:47:17 +0900

    [update]少し更新

--- Assets/Project/Scripts/GyroController.cs
+++ Assets/Project/Scripts/GyroController.cs
@@ -1,23 +1,22 @@
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using static JSL;
 
 public class GyroController : MonoBehaviour
 {
     [SerializeField] private DeviceConnectManager _deviceConnectManager;
+    private Rigidbody _rigidbody;
+
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         int usingHandle = _deviceConnectManager.SelectedHandle;
+
         IMU_STATE imu = JslGetIMUState(usingHandle);
         MOTION_STATE motion = JslGetMotionState(usingHandle);
-        //Debug.Log($"加速度１{imu.accelX}, {imu.accelY}, {imu.accelZ}");
-        //Debug.Log($"加速度２{motion.accelX}, {motion.accelY}, {motion.accelZ}");
-        //Debug.Log($"ジャイロ{imu.gyroX}, {imu.gyroY}, {imu.gyroZ}");
-        Debug.Log($"クオータニオン{motion.quatW}, {motion.quatX}, {motion.quatY}, {motion.quatZ}");
-        //Debug.Log($"重力方向{motion.gravX}, {motion.gravY}, {motion.gravZ}");
     }
 }
