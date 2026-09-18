aa6eec2297be6fcac48172520208e74b0297c75c
puchuanyuan9 <puchuanyuan9@gmail.com>
2026-09-18 15:24:02 +0900

    [update]障害物が透ける仕組み完成、ギミックを追加

--- Assets/Project/Scripts/MazeCutOutController.cs
+++ Assets/Project/Scripts/MazeCutOutController.cs
@@ -5,10 +5,12 @@ public class MazeCutOutController : MonoBehaviour
     [SerializeField] Camera _mainCamera;
     [SerializeField] Transform _ballTransform;
     [SerializeField] Material _mazeCutOutMaterial;
+    [SerializeField, Range(1.0f, 3.0f)] private float _adjustDifference = 1.5f;
 
     private void LateUpdate()
     {
         Vector3 ballViewportPosition = _mainCamera.WorldToViewportPoint(_ballTransform.position);
         _mazeCutOutMaterial.SetVector("_HoleCenter", new Vector4(ballViewportPosition.x, ballViewportPosition.y, 0, 0));
+        _mazeCutOutMaterial.SetFloat("_BallDepth", ballViewportPosition.z - _adjustDifference);
     }
 }
